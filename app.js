2f041c5c509af4f2a3a99bb0ec9973cb0c59a507
Richard Rosario <tech2js@gmail.com>
2020-07-05 13:58:12 +0800

    working on ajax http requests

--- app.js
+++ app.js
@@ -163,3 +163,24 @@ function addColor(time, element, color) {
     }
   });
 }
+
+const xhr = new XMLHttpRequest();
+const result = document.querySelector(".result");
+
+// open the file
+xhr.open("GET", "./api.txt");
+xhr.onreadystatechange = function () {
+  console.log(xhr);
+  if (xhr.readyState === 4 || xhr.status === 200) {
+    const text = document.createElement("p");
+    text.textContent = xhr.responseText;
+    result.appendChild(text);
+  } else {
+    console.log({
+      status: xhr.status,
+      text: xhr.responseText,
+    });
+  }
+};
+
+xhr.send();
