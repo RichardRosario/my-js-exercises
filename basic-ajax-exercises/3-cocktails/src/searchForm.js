b6f13bcbccbc02554ad4f2beb595c78be96138cc
Richard Rosario <tech2js@gmail.com>
2020-07-08 12:09:20 +0800

    show drinks

--- basic-ajax-exercises/3-cocktails/src/searchForm.js
+++ basic-ajax-exercises/3-cocktails/src/searchForm.js
@@ -1,5 +1,5 @@
 import getElement from "./getElement.js";
-import presentDrinks from "./presentDrinks.js";
+import showDrinks from "./presentDrinks.js";
 
 const baseURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
@@ -10,5 +10,5 @@ form.addEventListener("keyup", (e) => {
   e.preventDefault();
   const value = input.value;
   if (!input.value) return;
-  presentDrinks(`${baseURL}${value}`);
+  showDrinks(`${baseURL}${value}`);
 });
