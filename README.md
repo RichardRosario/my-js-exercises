b2d2990a8cbc34f96bd5d0e4decb6c161c5e4a12
Richard Rosario <tech2js@gmail.com>
2020-07-12 16:51:41 +0800

    equalities

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ### This is my javascript workout. As I am sharpening my javascript skills which has been dull and untapped in the last 2 years.. Just like in any skills in life.. for example sports, one need to hone it constantly.. prolong absence dulls those skills.. and I am not shying away from saying that I am not where I should be or used to be.
 
-Javascript is my first love.. but due to circumstances, I couldn't and didnt have the opportunity to reallyand fully work on it in my previous jobs..
+Javascript is my first love.. but due to circumstances, I couldn't and didnt have the opportunity to really and fully work on it in my previous jobs..
 
 Now that I am one of the casualty of this covid19.. i get the time to sharpen my javascript skills again..
 
--- app.js
+++ app.js
@@ -135,61 +135,8 @@
 //   await
 // }
 
-const heading1 = document.querySelector(".one");
-const heading2 = document.querySelector(".two");
-const heading3 = document.querySelector(".three");
-
-const btn = document.querySelector(".btn");
-
-btn.addEventListener("click", async () => {
-  try {
-    await addColor(2000, heading1, "red");
-    await addColor(2000, heading2, "green");
-    await addColor(1000, heading3, "blue");
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-function addColor(time, element, color) {
-  return new Promise((resolve, reject) => {
-    if (element) {
-      setTimeout(() => {
-        element.style.color = color;
-        resolve();
-      }, time);
-    } else {
-      reject(new Error(`cannot add element ${element}`));
-    }
-  });
-}
-// ajax-http
-
-const btn2 = document.querySelector(".btn2");
-btn2.addEventListener("click", function () {
-  getData();
-});
-
-function getData() {
-  const xhr = new XMLHttpRequest();
-  const result = document.querySelector(".result");
-
-  // open the file
-  xhr.open("GET", "./api.txt");
-  xhr.onreadystatechange = function () {
-    console.log(xhr);
-    if (xhr.readyState === 4 || xhr.status === 200) {
-      const text = document.createElement("p");
-      text.textContent = xhr.responseText;
-      result.appendChild(text);
-    } else {
-      console.log({
-        status: xhr.status,
-        text: xhr.responseText,
-        state: xhr.readyState,
-      });
-    }
-  };
-
-  xhr.send();
-}
+//== vs ===
+let r = 8;
+const r1 = "8";
+r == "8" ? console.log("true") : console.log("false"); //true
+r === "8" ? console.log("true") : console.log("false"); //false
